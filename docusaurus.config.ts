0906d5dab5d9da6b4e6a6fcbed87982a86ddbc41
rypervenche <git@ryper.org>
2025-02-22 18:04:04 -0600

    Show all posts in blog

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -63,6 +63,8 @@ const config: Config = {
           onInlineTags: 'warn',
           onInlineAuthors: 'warn',
           onUntruncatedBlogPosts: 'warn',
+          blogSidebarTitle: 'All posts',
+          blogSidebarCount: 'ALL',
         },
         theme: {
           customCss: './src/css/custom.css',
