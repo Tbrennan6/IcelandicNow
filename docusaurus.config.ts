ff8123e0061d8eaa8da9b59f75bcd56ae415079e
Tbrennan6 <tjb0827@gmail.com>
2025-06-17 22:14:36 -0500

    change preview image hopefully

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -75,7 +75,7 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: 'img/icelandicnow_sheep.png',
     navbar: {
       title: 'Home',
       logo: {
