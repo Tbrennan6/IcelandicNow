b98a89c86a87a2cf5f831794952a3b6db6f22fb0
rypervenche <git@ryper.org>
2026-07-02 15:43:36 -0500

    Add flake.nix file for development

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "Docusaurus environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          nodejs
+        ];
+      };
+    };
+}
